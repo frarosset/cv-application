e611926b860266b03a9452ab96707ee1c4579a0c
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-23 15:13:05 +0100

    feat(PreviewPersonalInfoPanel): add name, surname, position, and aboutMe Text properties
    Such properties are retrieved from props.personalInfo derived from the
    Main component. Some dummy data are initialized in there, for testing
    purposes.

--- src/components/PreviewPersonalInfoPanel.jsx
+++ src/components/PreviewPersonalInfoPanel.jsx
@@ -1,8 +1,13 @@
+import Text from "./Text.jsx";
+const textProperties = ["name", "surname", "title", "aboutMe"];
+
 function PreviewPersonalInfoPanel({ personalInfo }) {
   return (
     <div className="preview-personal-info-panel">
       <h3>Personal Info</h3>
-      {personalInfo.test}
+      {textProperties.map((prop) => (
+        <Text key={prop} customClass={prop} value={personalInfo[prop]} />
+      ))}
     </div>
   );
 }
