92857167b74d9922ba399cb541c0209a09960dd6
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-23 21:48:41 +0100

    feat(PreviewPersonalInfoPanel): add "location", "email", "phone", "website", "linkedin", "github" info
    Those info are added as TextWithIcon components. Data are retrieved from
    prop.personalInfo which derives from the state initialized in Main
    component.
    
    Such components are wrapped in a div.

--- src/components/PreviewPersonalInfoPanel.jsx
+++ src/components/PreviewPersonalInfoPanel.jsx
@@ -1,5 +1,15 @@
 import Text from "./Text.jsx";
+import TextWithIcon from "./TextWithIcon.jsx";
+
 const textProperties = ["name", "surname", "title", "aboutMe"];
+const textWithIconsProperties = [
+  "location",
+  "email",
+  "phone",
+  "linkedin",
+  "website",
+  "github",
+];
 
 function PreviewPersonalInfoPanel({ personalInfo }) {
   return (
@@ -8,6 +18,22 @@ function PreviewPersonalInfoPanel({ personalInfo }) {
       {textProperties.map((prop) => (
         <Text key={prop} customClass={prop} value={personalInfo[prop]} />
       ))}
+
+      <div className="contact-list">
+        {textWithIconsProperties.map((prop) => {
+          const value = personalInfo[prop];
+          return (
+            value && (
+              <TextWithIcon
+                key={prop}
+                iconName={prop}
+                customClass={prop}
+                value={value}
+              />
+            )
+          );
+        })}
+      </div>
     </div>
   );
 }
