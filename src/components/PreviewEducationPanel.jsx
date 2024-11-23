84a934303c9fcbf41c06a1db45bbceab6454014a
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-23 03:58:13 +0100

    feat(PreviewEducationPanel): initialize PreviewEducationPanel component
    The component currently just shows an h3 heading wrapped into a div.
    
    The component is set in the PreviewPanel component.

---
+++ src/components/PreviewEducationPanel.jsx
@@ -0,0 +1,9 @@
+function PreviewEducationPanel() {
+  return (
+    <div className="preview-education-panel">
+      <h3>Education</h3>
+    </div>
+  );
+}
+
+export default PreviewEducationPanel;
--- src/components/PreviewPanel.jsx
+++ src/components/PreviewPanel.jsx
@@ -1,10 +1,12 @@
 import PreviewPersonalInfoPanel from "./PreviewPersonalInfoPanel.jsx";
+import PreviewEducationPanel from "./PreviewEducationPanel.jsx";
 
 function PreviewPanel() {
   return (
     <div className="preview-panel">
       <h2>Preview Panel</h2>
       <PreviewPersonalInfoPanel />
+      <PreviewEducationPanel />
     </div>
   );
 }
