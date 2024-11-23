ed58d58aa7e0fa5a22a999a4f78e208cbc6d06ba
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-23 02:29:46 +0100

    feat(EditPersonalInfoPanel): initialize EditPersonalInfoPanel component
    The component currently just shows an h3 heading wrapped into a div.
    
    The component is set in the EditPanel component.

--- src/components/EditPanel.jsx
+++ src/components/EditPanel.jsx
@@ -1,7 +1,10 @@
+import EditPersonalInfoPanel from "./EditPersonalInfoPanel.jsx";
+
 function EditPanel() {
   return (
     <div className="edit-panel">
       <h2>Edit Panel</h2>
+      <EditPersonalInfoPanel />
     </div>
   );
 }
---
+++ src/components/EditPersonalInfoPanel.jsx
@@ -0,0 +1,9 @@
+function EditPersonalInfoPanel() {
+  return (
+    <div className="edit-personal-info-panel">
+      <h3>Edit Personal Info</h3>
+    </div>
+  );
+}
+
+export default EditPersonalInfoPanel;
