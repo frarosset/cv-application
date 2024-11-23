9b512dbcc0a426d675acb7d069f1ef1e66e042db
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-23 15:26:51 +0100

    feat(Main): initialize personalInfo state using some sampleData from a JSON file

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,10 @@
 import { useState } from "react";
 import EditPanel from "./EditPanel.jsx";
 import PreviewPanel from "./PreviewPanel.jsx";
+import sampleData from "../data/sampleData.json";
 
 function Main() {
-  const [personalInfo, setPersonalInfo] = useState({
-    name: "John",
-    surname: "Doe",
-    title: "Fullstack web developer",
-    aboutMe:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  });
+  const [personalInfo, setPersonalInfo] = useState(sampleData.personalInfo);
 
   return (
     <main>
