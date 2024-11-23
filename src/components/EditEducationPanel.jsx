2ee4253a3b84f424eb2134026454d87cb59bbc2c
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-23 02:33:02 +0100

    feat(EditEducationPanel): initialize EditEducationPanel component
    The component currently just shows an h3 heading wrapped into a div..
    
    The component is set in the EditPanel component.

---
+++ src/components/EditEducationPanel.jsx
@@ -0,0 +1,9 @@
+function EditEducationPanel() {
+  return (
+    <div className="edit-education-panel">
+      <h3>Edit Education</h3>
+    </div>
+  );
+}
+
+export default EditEducationPanel;
--- src/components/EditPanel.jsx
+++ src/components/EditPanel.jsx
@@ -1,10 +1,12 @@
 import EditPersonalInfoPanel from "./EditPersonalInfoPanel.jsx";
+import EditEducationPanel from "./EditEducationPanel.jsx";
 
 function EditPanel() {
   return (
     <div className="edit-panel">
       <h2>Edit Panel</h2>
       <EditPersonalInfoPanel />
+      <EditEducationPanel />
     </div>
   );
 }
