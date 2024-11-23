c34bd072ce7f584357dc0c05599933a6e433be99
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-23 17:42:25 +0100

    feat(EditPersonalInfoPanel): add InputWithLabel components for name, surname, position, and aboutMe properties
    The initial value of such properties are retrieved from
    props.personalInfo derived from the Main component.
    
    The props.setPersonalInfo is also used to modify such state on input
    change.
    
    The specification of the correct "label", "maxLength", "required",
    "type", and "placeholder" for each input will be implemented next.

--- src/components/EditPersonalInfoPanel.jsx
+++ src/components/EditPersonalInfoPanel.jsx
@@ -1,8 +1,35 @@
+import InputWithLabel from "./InputWithLabel.jsx";
+
+const orderedInputProps = ["name", "surname", "title", "aboutMe"];
+
 function EditPersonalInfoPanel({ personalInfo, setPersonalInfo }) {
+  const setValueFor = (property) => (value) => {
+    setPersonalInfo({
+      ...personalInfo,
+      [property]: value,
+    });
+  };
+
   return (
     <div className="edit-personal-info-panel">
       <h3>Edit Personal Info</h3>
-      {personalInfo.test}
+
+      <form>
+        {orderedInputProps.map((prop) => (
+          <InputWithLabel
+            key={prop}
+            id={prop}
+            name={prop}
+            label="LABEL"
+            value={personalInfo[prop]}
+            placeholder="PLACEHOLDER"
+            setValue={setValueFor(prop)}
+            type="text"
+            maxLength="30"
+            required={false}
+          />
+        ))}
+      </form>
     </div>
   );
 }
