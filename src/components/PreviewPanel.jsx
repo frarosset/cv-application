d0d3b478b4cb04569b0b4aa426a60fb4fa186645
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-23 02:42:19 +0100

    feat(PreviewPersonalInfoPanel): initialize PreviewPersonalInfoPanel component
    The component currently just shows an h3 heading wrapped into a div.
    
    The component is set in the PreviewPanel component.

--- src/components/PreviewPanel.jsx
+++ src/components/PreviewPanel.jsx
@@ -1,7 +1,10 @@
+import PreviewPersonalInfoPanel from "./PreviewPersonalInfoPanel.jsx";
+
 function PreviewPanel() {
   return (
     <div className="preview-panel">
       <h2>Preview Panel</h2>
+      <PreviewPersonalInfoPanel />
     </div>
   );
 }
---
+++ src/components/PreviewPersonalInfoPanel.jsx
@@ -0,0 +1,9 @@
+function PreviewPersonalInfoPanel() {
+  return (
+    <div className="preview-personal-info-panel">
+      <h3>Personal Info</h3>
+    </div>
+  );
+}
+
+export default PreviewPersonalInfoPanel;
