fb17fc6c4e63df1ff436574cbebf07445a691ccf
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-24 16:19:05 +0100

    style(Main): add style to Main component
    Only the "top-level" style is defined. The style of the specific
    components used in the Main component are styled in their own module.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import EditPanel from "./editor/EditPanel.jsx";
 import PreviewPanel from "./preview/PreviewPanel.jsx";
 import sampleData from "../data/sampleData.json";
+import "../styles/Main.css";
 
 function Main() {
   const [personalInfo, setPersonalInfo] = useState(sampleData.personalInfo);
