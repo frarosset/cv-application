83c27f3e74f1a24db85466f4d53a1f8b482744f0
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-26 00:40:23 +0100

    style(PreviewPage): add style to PreviewPage component
    Only the "top-level" style is defined. The style of the specific
    components used in there component are styled in their own module.
    
    The aspect ratio has been set to that of an A4 paper. In this div,
    however, the content will not be split into multiple pages. A scroll
    will be enabled.
    
    The CSS property container-type has been set to inline-size. This way,
    it is possible to use sizes relative to this page size in the
    descendants elements. Although, note that older browser will not be
    supported.

--- src/components/preview/PreviewPage.jsx
+++ src/components/preview/PreviewPage.jsx
@@ -1,6 +1,7 @@
 import PreviewPersonalInfoPanel from "./PreviewPersonalInfoPanel.jsx";
 import PreviewEducationPanel from "./PreviewEducationPanel.jsx";
 import PreviewProfessionalExperience from "./PreviewProfessionalExperience.jsx";
+import "../../styles/preview/PreviewPage.css";
 
 function PreviewPage(props) {
   return (
