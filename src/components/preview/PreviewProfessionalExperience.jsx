b97125b4ddf09f73b7d3c02605076b727adc1ec3
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-26 23:47:55 +0100

    feat(PreviewProfessionalExperiencePanel): add PreviewListWithDates component to show professional experience data

--- src/components/preview/PreviewProfessionalExperience.jsx
+++ src/components/preview/PreviewProfessionalExperience.jsx
@@ -1,7 +1,17 @@
+import PreviewListWithDates from "./PreviewListWithDates.jsx";
+
+const textProperties = ["institution", "address", "role"];
+
 function PreviewProfessionalExperience({ professionalExperience }) {
   return (
     <div className="preview-professional-experience-panel">
       <h3>Professional Experience</h3>
+
+      <PreviewListWithDates
+        customClass={"professional-experience"}
+        data={professionalExperience}
+        textProperties={textProperties}
+      />
     </div>
   );
 }
