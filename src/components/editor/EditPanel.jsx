2257515f4ec749185c854557d005fe9c883e2724
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-28 17:42:13 +0100

    feat(EditPanel): add currentPanel state to this component and render only that Panel
    The setting of the currentPanel is not implemented, yet.

--- src/components/editor/EditPanel.jsx
+++ src/components/editor/EditPanel.jsx
@@ -1,26 +1,46 @@
+import { useState } from "react";
 import EditPersonalInfoPanel from "./EditPersonalInfoPanel.jsx";
 import EditEducationPanel from "./EditEducationPanel.jsx";
 import EditProfessionalExperience from "./EditProfessionalExperience.jsx";
 import "../../styles/editor/EditPanel.css";
 
 function EditPanel(props) {
+  const [currentPanel, setCurrentPanel] = useState("personal-info");
+
   return (
     <div className="edit-panel">
       <h2>Edit Panel</h2>
-      <EditPersonalInfoPanel
-        personalInfo={props.personalInfo}
-        setPersonalInfo={props.setPersonalInfo}
-      />
-      <EditEducationPanel
-        education={props.education}
-        setEducation={props.setEducation}
-      />
-      <EditProfessionalExperience
-        professionalExperience={props.professionalExperience}
-        setProfessionalExperience={props.setProfessionalExperience}
-      />
+      {renderCurrentPanel(currentPanel, props)}
     </div>
   );
+}
+
+function renderCurrentPanel(currentPanel, props) {
+  switch (currentPanel) {
+    case "personal-info":
+      return (
+        <EditPersonalInfoPanel
+          personalInfo={props.personalInfo}
+          setPersonalInfo={props.setPersonalInfo}
+        />
+      );
+    case "education":
+      return (
+        <EditEducationPanel
+          education={props.education}
+          setEducation={props.setEducation}
+        />
+      );
+    case "professional-experience":
+      return (
+        <EditProfessionalExperience
+          professionalExperience={props.professionalExperience}
+          setProfessionalExperience={props.setProfessionalExperience}
+        />
+      );
+    default:
+      return null;
+  }
 }
 
 export default EditPanel;
