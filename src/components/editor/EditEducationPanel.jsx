78cb5ea04b23f3272348186013eb6976dbed6c58
frarosset <37275581+frarosset@users.noreply.github.com>
2024-11-29 17:55:43 +0100

    feat(EditEducationPanel): add EditListSelectionButtons component in this component
    The list of items associated with the EditListSelectionButtons is
    prop.education.
    
    Currently, just a short text showing the currently selected item is
    shown after this EditListSelectionButtons component. It will be
    substituted by a form to set a new item or edit it.

--- src/components/editor/EditEducationPanel.jsx
+++ src/components/editor/EditEducationPanel.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+import EditListSelectionButtons from "./EditListSelectionButtons.jsx";
 import "../../styles/editor/EditSectionPanel.css";
 
 function EditEducationPanel({ education, setEducation }) {
+  const [currentItemId, setCurrentItemId] = useState(null);
+
   return (
     <div className="edit-section-panel edit-education-panel">
       <h3>Edit Education</h3>
+      <EditListSelectionButtons
+        list={education}
+        currentItemId={currentItemId}
+        setCurrentItemId={setCurrentItemId}
+      />
+      <p>
+        {currentItemId != null &&
+          `${currentItemId} ${
+            currentItemId !== -1 && education[currentItemId].degree
+          }, ${currentItemId !== -1 && education[currentItemId].institution}`}
+      </p>
     </div>
   );
 }
