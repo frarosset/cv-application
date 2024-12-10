84cae6a4991294c5c72d8993b6342fa4eaef76f0
frarosset <37275581+frarosset@users.noreply.github.com>
2024-12-10 17:19:10 +0100

    feat(DateInputWithLabel): add DateInputWithLabel component
    This component contains two selectInput component, one to select a
    month, and one to select a year.
    
    An optional range can be specified, providing the values for the minimum
    date (fromMonth, fromYear) and maximum date (toMonth, toYear).
    
    The intervals for both the possible months and years are then computed
    based on the minimum, maximum, and selected dates, computing the
    firstYear, lastYear, firstMonth, and lastMonth variables. If some of
    these  values are missing, some default values are used (1900,
    currentYear, January, and December, respectively).
    
    Note that those values are computed so that it is not possible to select
    an invalid date. Eg, if:
        - the minimum date is March 2022
        - the maximum date is October 2024
        - the current date is February 2023
    we get:
        - firstYear: 2023
        - lastYear: 2024
        - firstMonth: January
        - lastMonth: December
    Recall that you cannot change the month and the year simultaneously, so:
        - any month is valid if you change the month (keeping year 2023)
        - year 2022 would not be selectable (keeping month February),
          because February 2022 is out of range.
    
    Other props that can be specified include: id, name, valueMonth and
    valueYear (the current controlled month a year), setMonth and setYear
    (to set such values), a required flag, and a label.
    
    Note that a single label is used for both the month and year, as they
    are essetntially both part of a label. Both the month and year select
    elements are however labeled using "aria-label" property.
    
    This component is different from the <input type="month"/> element,
    because the month can be specified as empty, selecting in fact just a
    year.

---
+++ src/components/base/DateInputWithLabel.jsx
@@ -0,0 +1,146 @@
+import SelectInput from "./SelectInput.jsx";
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+function DateInputWithLabel({
+  id,
+  name,
+  valueMonth,
+  valueYear,
+  setMonth,
+  setYear,
+  required = false,
+  label,
+  fromMonth,
+  fromYear,
+  toMonth,
+  toYear,
+}) {
+  const idMonth = `${id}Month`;
+  const idYear = `${id}Year`;
+  const nameMonth = `${name}Month`;
+  const nameYear = `${name}Year`;
+
+  const { firstYear, lastYear, firstMonth, lastMonth } = computeConstraints(
+    valueMonth,
+    valueYear,
+    fromMonth,
+    fromYear,
+    toMonth,
+    toYear
+  );
+
+  const monthInput = (
+    <SelectInput
+      id={idMonth}
+      name={nameMonth}
+      value={valueMonth}
+      options={optionsMonth(firstMonth, lastMonth)}
+      emptyValue={""}
+      setValue={setMonth}
+      placeholder={"month"}
+      required={required}
+      aria-label={`${label} month`}
+    />
+  );
+
+  const yearInput = (
+    <SelectInput
+      id={idYear}
+      name={nameYear}
+      value={valueYear}
+      options={optionsYear(firstYear, lastYear)}
+      setValue={setYear}
+      placeholder={"year"}
+      required={required}
+      aria-label={`${label} year`}
+    />
+  );
+
+  return (
+    <div className="input-with-label date-input-with-label">
+      <label htmlFor={idYear}>{label}</label>
+      {monthInput}
+      {yearInput}
+    </div>
+  );
+}
+
+function computeConstraints(
+  valueMonth,
+  valueYear,
+  fromMonth,
+  fromYear,
+  toMonth,
+  toYear
+) {
+  const isToMonthAntecedent = getMonthIdx(valueMonth) > getMonthIdx(toMonth);
+  const isFromMonthConsequent =
+    getMonthIdx(fromMonth) > getMonthIdx(valueMonth);
+
+  const firstYear =
+    (fromYear != null ? Number(fromYear) : 1900) +
+    (isFromMonthConsequent ? 1 : 0);
+  const lastYear =
+    (toYear != null ? Number(toYear) : new Date().getFullYear()) -
+    (isToMonthAntecedent ? 1 : 0);
+
+  const isFirstYear =
+    valueYear != null && valueYear == firstYear && !isFromMonthConsequent;
+  const isLastYear =
+    valueYear != null && valueYear == lastYear && !isToMonthAntecedent;
+
+  const firstMonth = isFirstYear && fromMonth ? fromMonth : months[0];
+  const lastMonth = isLastYear && toMonth ? toMonth : months[months.length - 1];
+
+  return {
+    firstYear,
+    lastYear,
+    firstMonth,
+    lastMonth,
+  };
+}
+
+function range(start, end, step = 1) {
+  const length = Math.floor((end - start) / step) + 1;
+  // see https://stackoverflow.com/a/28247338
+  return Array.from({ length }, (v, k) => start + k * step);
+}
+
+// this is to get the index of the months in O(1), avoiding a .findIndex() call
+const monthsIdxMap = new Map();
+months.forEach((key, idx) => monthsIdxMap.set(key, idx));
+
+function getMonthIdx(month) {
+  return monthsIdxMap.get(month);
+}
+
+function optionsMonth(firstMonth, lastMonth) {
+  const firstMonthIdx = getMonthIdx(firstMonth);
+  const lastMonthIdx = getMonthIdx(lastMonth);
+
+  const sliceFrom = firstMonthIdx ? firstMonthIdx : 0;
+  /* sliceTo includes the +1 to point to the exclusive last item to select */
+  const sliceTo = lastMonthIdx ? lastMonthIdx + 1 : months.length;
+
+  return months.slice(sliceFrom, sliceTo);
+}
+
+function optionsYear(firstYear, lastYear) {
+  return [...range(lastYear, firstYear, -1)];
+}
+
+export default DateInputWithLabel;
