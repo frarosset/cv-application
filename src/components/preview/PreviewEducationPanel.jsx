1481dafe448236b25bf3176209e532ce7be0f9dc
frarosset <37275581+frarosset@users.noreply.github.com>
2024-12-20 14:48:41 +0100

    fix(PreviewEducationPanel, PreviewProfessionalExperience): do not show Education / Professional Experience sections if they have no items

--- src/components/preview/PreviewEducationPanel.jsx
+++ src/components/preview/PreviewEducationPanel.jsx
@@ -4,17 +4,19 @@ import "../../styles/preview/previewPanelWithListAndDates.css";
 const textProperties = ["institution", "address", "degree"];
 
 function PreviewEducationPanel({ education }) {
-  return (
-    <div className="preview-panel-with-list-and-dates preview-education-panel">
-      <h3>Education</h3>
+  if (education.length > 0) {
+    return (
+      <div className="preview-panel-with-list-and-dates preview-education-panel">
+        <h3>Education</h3>
 
-      <PreviewListWithDates
-        customClass={"education"}
-        data={education}
-        textProperties={textProperties}
-      />
-    </div>
-  );
+        <PreviewListWithDates
+          customClass={"education"}
+          data={education}
+          textProperties={textProperties}
+        />
+      </div>
+    );
+  }
 }
 
 export default PreviewEducationPanel;
--- src/components/preview/PreviewProfessionalExperience.jsx
+++ src/components/preview/PreviewProfessionalExperience.jsx
@@ -4,17 +4,19 @@ import "../../styles/preview/previewPanelWithListAndDates.css";
 const textProperties = ["institution", "address", "role"];
 
 function PreviewProfessionalExperience({ professionalExperience }) {
-  return (
-    <div className="preview-panel-with-list-and-dates preview-professional-experience-panel">
-      <h3>Professional Experience</h3>
+  if (professionalExperience.length > 0) {
+    return (
+      <div className="preview-panel-with-list-and-dates preview-professional-experience-panel">
+        <h3>Professional Experience</h3>
 
-      <PreviewListWithDates
-        customClass={"professional-experience"}
-        data={professionalExperience}
-        textProperties={textProperties}
-      />
-    </div>
-  );
+        <PreviewListWithDates
+          customClass={"professional-experience"}
+          data={professionalExperience}
+          textProperties={textProperties}
+        />
+      </div>
+    );
+  }
 }
 
 export default PreviewProfessionalExperience;
