36ef84121a177c8203c7a1061484fc6322fb3651
frarosset <37275581+frarosset@users.noreply.github.com>
2024-12-20 12:03:52 +0100

    feat(ArrayOfInputWithLabel): add support for DateInputWithLabel and IntervalDateInputWithLabels input items
    Return a specific input component based on inputProps[prop].type.
    In particular:
    
    - if the type is "date-month-year", instantiate a DateInputWithLabel
      component;
    
    - if the type is "date-month-year-interval", instantiate a
      IntervalDateInputWithLabels component;
    
    - else, instantiate a InputWithLabel component.

--- src/components/base/ArrayOfInputWithLabel.jsx
+++ src/components/base/ArrayOfInputWithLabel.jsx
@@ -1,4 +1,6 @@
 import InputWithLabel from "./InputWithLabel.jsx";
+import DateInputWithLabel from "../base/DateInputWithLabel.jsx";
+import IntervalDateInputWithLabels from "../base/IntervalDateInputWithLabels.jsx";
 
 function ArrayOfInputWithLabel({
   orderedInputProps,
@@ -8,20 +10,51 @@ function ArrayOfInputWithLabel({
 }) {
   return (
     <>
-      {orderedInputProps.map((prop) => (
-        <InputWithLabel
-          key={prop}
-          id={prop}
-          name={prop}
-          label={inputProps[prop].label}
-          value={item[prop]}
-          placeholder={inputProps[prop].placeholder}
-          setValue={setValueFor(prop)}
-          type={inputProps[prop].type}
-          maxLength={inputProps[prop].maxLength}
-          required={inputProps[prop].required}
-        />
-      ))}
+      {orderedInputProps.map((prop) => {
+        if (inputProps[prop].type === "date-month-year") {
+          return (
+            <DateInputWithLabel
+              key={prop}
+              id={prop}
+              name={prop}
+              label={inputProps[prop].label}
+              valueMonth={item[`${prop}Month`]}
+              valueYear={item[`${prop}Year`]}
+              setMonth={setValueFor(`${prop}Month`)}
+              setYear={setValueFor(`${prop}Year`)}
+              firstYear={inputProps[prop].firstYear}
+              firstMonth={inputProps[prop].firstMonth}
+              lastYear={inputProps[prop].lastYear}
+              lastMonth={inputProps[prop].lastMonth}
+              required={inputProps[prop].required}
+            />
+          );
+        } else if (inputProps[prop].type === "date-month-year-interval") {
+          return (
+            <IntervalDateInputWithLabels
+              key={prop}
+              fromDateProp={inputProps[prop].fromDateProp}
+              toDateProp={inputProps[prop].toDateProp}
+              {...{ item, inputProps, setValueFor }}
+            />
+          );
+        } else {
+          return (
+            <InputWithLabel
+              key={prop}
+              id={prop}
+              name={prop}
+              label={inputProps[prop].label}
+              value={item[prop]}
+              placeholder={inputProps[prop].placeholder}
+              setValue={setValueFor(prop)}
+              type={inputProps[prop].type}
+              maxLength={inputProps[prop].maxLength}
+              required={inputProps[prop].required}
+            />
+          );
+        }
+      })}
     </>
   );
 }
