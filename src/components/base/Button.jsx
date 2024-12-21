441f01b7196b30961bdac01d7ae3046418ad81c1
frarosset <37275581+frarosset@users.noreply.github.com>
2024-12-21 17:30:21 +0100

    feat(Button): make it possible to set the type of button (default: type='button')

--- src/components/base/Button.jsx
+++ src/components/base/Button.jsx
@@ -1,10 +1,20 @@
 import Icon from "./Icon.jsx";
 
-function Button({ customCssClass, iconName, text, onClickCallback }) {
+function Button({
+  customCssClass,
+  iconName,
+  text,
+  onClickCallback,
+  type = "button",
+}) {
   const icon = iconName != null && <Icon name={iconName} />;
 
   return (
-    <button className={`button ${customCssClass}`} onClick={onClickCallback}>
+    <button
+      className={`button ${customCssClass}`}
+      onClick={onClickCallback}
+      type={type}
+    >
       {icon}
       {text}
     </button>
