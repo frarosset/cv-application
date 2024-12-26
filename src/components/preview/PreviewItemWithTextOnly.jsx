b0ddfdd684bce6214acd482f45dd0b5e0e52df38
frarosset <37275581+frarosset@users.noreply.github.com>
2024-12-26 19:01:12 +0100

    style(PreviewItemWithTextOnly, PreviewListWithTextOnly): add style specific to preview skills and languages panels

--- src/components/preview/PreviewItemWithTextOnly.jsx
+++ src/components/preview/PreviewItemWithTextOnly.jsx
@@ -5,9 +5,12 @@ function PreviewItemWithTextOnly({ itemData, customClass, textProperties }) {
   return (
     <li className={`preview-item-with-text-only ${customClass}`}>
       <div className={`preview-item-with-text-only-text-blocks`}>
-        {textProperties.map((prop) => (
-          <Text key={prop} customClass={prop} value={itemData[prop]} />
-        ))}
+        {textProperties.map(
+          (prop) =>
+            itemData[prop] && (
+              <Text key={prop} customClass={prop} value={itemData[prop]} />
+            )
+        )}
       </div>
     </li>
   );
