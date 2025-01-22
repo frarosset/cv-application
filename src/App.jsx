e28530b82a29e55af07789db524bbbf9433d64da
frarosset <37275581+frarosset@users.noreply.github.com>
2025-01-22 12:45:30 +0100

    feat(CreditFooter): add CreditFooter component
    This component is just a footer with a link to my Github page.
    
    It has been inserted in the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
+import CreditFooter from "./components/base/CreditFooter.jsx";
 
 function App() {
   const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
   const [darkThemeSet, setDarkThemeSet] = useState(defaultDark);
 
   return (
-    <div className={`app-container ${darkThemeSet && "dark-theme"}`}>
-      <Header {...{ darkThemeSet, setDarkThemeSet }} />
-      <Main />
-    </div>
+    <>
+      <div className={`app-container ${darkThemeSet ? "dark-theme" : ""}`}>
+        <Header {...{ darkThemeSet, setDarkThemeSet }} />
+        <Main />
+        <CreditFooter darkTheme={darkThemeSet} />
+      </div>
+    </>
   );
 }
 
